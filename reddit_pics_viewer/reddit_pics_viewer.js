e9daa194b94f319838b9e657d9161cdf603f2905
Paul Koepke <paul.koepke@gmail.com>
2015-03-27 09:58:39 -0400

    Removed large webm file, replaced with several much-smaller files.

--- reddit_pics_viewer/reddit_pics_viewer.js
+++ reddit_pics_viewer/reddit_pics_viewer.js
@@ -3,7 +3,7 @@
 // JavaScript / JQuery for the Reddit Pics Viewer web page. HTML file must include JQuery with this line: <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 // Tumbeasts by Matthew Inman are licensed under a Creative Commons Attribution 3.0 Unported License. Based on a work at theoatmeal.com.
 // Wikipedia edit 2014 video by VGrias is licensed under the Creative Commons Attribution-Share Alike 3.0 Unported license. http://commons.wikimedia.org/wiki/File:Wikipedia_Edit_2014.webm
-// Creative Commons video by Peteforsyth is licensed under the Creative Commons Attribution 3.0 Unported license. http://commons.wikimedia.org/wiki/File:CreativeCommons.webm
+// Triple conj.ogg by Tos is licensed under the Creative Commons Attribution-Share Alike 3.0 Unported license. http://commons.wikimedia.org/wiki/File:Triple_conj.ogg
 
 // Test lines
 // document.getElementById("paragraph_for_video").innerHTML = "Updated by plain JavaScript"; // for testing only
@@ -13,13 +13,14 @@
 $( "#image_tag" ).attr( "src", "oatmeal_tumbeasts/tbservers.png" );
 
 // As soon as the page loads, add the default video to the Gfycat tag: <source id="source_tag_for_video" src="https://upload.wikimedia.org/wikipedia/commons/4/4d/Wikipedia_Edit_2014.webm" type="video/webm" />. If the video appears, JQuery is working correctly.
-var default_video = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Wikipedia_Edit_2014.webm"
-default_video = "http://fat.gfycat.com/BaggyJollyCaribou.webm"
-default_video = "CreativeCommons.webm"
-$( "#source_tag_for_video" ).attr( "src", default_video );
+var default_video = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Wikipedia_Edit_2014.webm";
+default_video = "http://fat.gfycat.com/BaggyJollyCaribou.webm";
+default_video = "Triple_conj_VP8_Vorbis.webm";
+// $( "#source_tag_for_video" ).attr( "src", default_video );
+$( "#responsive_div_for_video" ).html('<video id="video_tag" autoplay loop controls muted="muted" class="embed-responsive-item" > <source id="source_tag_for_video" src="" type="video/webm" /></video>');
 
 //My modification:
-jsonUrl = "http://www.reddit.com/r/gifs.json?jsonp=jQuery"
+jsonUrl = "http://www.reddit.com/r/gifs.json?jsonp=jQuery";
 $.ajax({
   url: jsonUrl,
   dataType: 'jsonp',
