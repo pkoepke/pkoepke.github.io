b9c1943992bd4863c933b1e1d84d3307a14d1c60
Paul Koepke <paul.koepke@gmail.com>
2015-03-30 19:35:08 -0400

    Got JSON getting working.

--- reddit_pics_viewer/reddit_pics_viewer.js
+++ reddit_pics_viewer/reddit_pics_viewer.js
@@ -1,5 +1,3 @@
-<!--
-
 /* Test pages:
 http://gfycat.com/BaggyJollyCaribou
 http://imgur.com/gallery/I3cBLuV
@@ -24,8 +22,16 @@ default_video = "Triple_conj_VP8_Vorbis.webm";
 // $( "#source_tag_for_video" ).attr( "src", default_video );
 $( "#responsive_div_for_video" ).html('<video id="video_tag" autoplay loop controls muted="muted" class="embed-responsive-item" > <source id="source_tag_for_video" src="Triple_conj_VP8_Vorbis.webm" type="video/webm" /></video>');
 
+// When the Load Subreddit button is clicked, grab the JSON from that subreddit.
+function load_subreddit() {
+	var subreddit_to_load = $( "#subreddit_input" ).val();
+	// console.log( subreddit_to_load ); // for testing only
+	$( "#span_for_output_to_user" ).html( "Loading..." )
+	grab_JSON( subreddit_to_load );
+}
+
 //My modification:
-jsonUrl = "https://www.reddit.com/r/gifs.json?jsonp=jQuery";
+/* jsonUrl = "https://www.reddit.com/r/gifs.json?jsonp=jQuery";
 console.log("Getting JSON from " + jsonUrl)
 redditJSON = $.ajax({
   url: jsonUrl,
@@ -36,6 +42,51 @@ redditJSON = $.ajax({
   timeout: 5
 })
 
-document.write( redditJSON.toSource() )
+document.write( redditJSON.toSource() ) */
 
--->
+// Another example from GitHub: https://gist.github.com/sente/947491
+/* $.getJSON( "http://www.reddit.com/r/gifs.json",
+  function foo(data) {
+  // console.log(data) // for testing only
+    $.each(data.data.children,
+	  function (i, post) {
+		$("#append_individual_posts").append( '<br>' + post.data.title );
+        $("#append_individual_posts").append( '<br>' + post.data.title );
+        $("#append_individual_posts").append( '<br>' + post.data.url );
+        $("#append_individual_posts").append( '<br>' + post.data.permalink );
+        $("#append_individual_posts").append( '<br>' + post.data.ups );
+        $("#append_individual_posts").append( '<br>' + post.data.downs );
+        $("#append_individual_posts").append( '<hr>' );
+      }
+    )
+  }
+)
+      .success(function() { alert("second success"); })
+      .error(function() { alert("error"); })
+      .complete(function() { alert("complete"); })
+*/
+
+// Function to grab JSON once the subreddit has been chosen. Modified from another example from StackOverflow: http://stackoverflow.com/questions/1739800/variables-set-during-getjson-function-only-accessible-within-function
+var redditJSON = {}
+function grab_JSON( subreddit_to_load ) {
+  subreddit_URL = "http://www.reddit.com/r/" + subreddit_to_load + ".json";
+  $.ajax ({
+    url: subreddit_URL,
+    async: false,
+    dataType:'json',
+    timeout: 30000,
+    success: function(data) {
+      redditJSON = data;
+      // console.log( data ); // for testing only
+	  $( "#span_for_output_to_user" ).html( "Finished loading." )
+    }, 
+    error: function(error){console.log("AJAX error:",error); redditJSON = {}; $( "#span_for_output_to_user" ).html( "Error - are you sure that's a subreddit?" )},
+    404: function(error){console.log("404 failed AJAX:",error); $( "#span_for_output_to_user" ).html( "404 error - couldn't get subreddit data." )},
+  })
+  console.log(redditJSON); // for testing only
+}
+
+// Function to display the image or video
+function (subredditJSON) {
+  
+}
