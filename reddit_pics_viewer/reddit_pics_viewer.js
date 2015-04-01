3340112a86db01220ce09e70033bb831034f573b
unknown <ptkoepke@PTKOEPKE150323.huronconsultinggroup.com>
2015-04-01 09:58:34 -0400

    Started working on getting imgur and gfycat pages. May be impossible due to cross-domain issues. In progress but moving back to desktop so committing, pushing, and pulling.

--- reddit_pics_viewer/reddit_pics_viewer.js
+++ reddit_pics_viewer/reddit_pics_viewer.js
@@ -14,9 +14,9 @@ http://imgur.com/gallery/I3cBLuV
  * Set up the page with default images and/or hide image and video elements until they're needed. See function initial_set_up()
  * When the user enters a subreddit and clicks the Load Subreddit button, grab the subreddit name and do an async $.ajax() call to grab the JSON from Reddit. See function load_subreddit(), which calls function grab_JSON( subreddit_to_load ).
  * Once the JSON is successfully gotten, immediately parse all of the URLs in data.data.children. Those from imgur.com or gfycat.com get put in an array that will be used to cycle through the images. See function parse_URLs().
- * When the array is ready, 
- * 
- * 
+ * When the array is ready, show the first image. See function display_content(), which is called by function grab_JSON( subreddit_to_load ).
+ * When the user clicks next or back, change the image # and redraw with display_content().
+ * If the image is 
  * 
  * 
 */
@@ -62,21 +62,12 @@ function grab_JSON( subreddit_to_load ) {
       // console.log( data ); // for testing only
       $( "#span_for_output_to_user" ).html( "Loaded." );
       parse_URLs();
-	  display_content();
+      display_content();
     }, 
     error: function(error){console.log("AJAX error:",error); redditJSON = {}; $( "#span_for_output_to_user" ).html( "Error - are you sure that's a subreddit?" )},
     404: function(error){console.log("404 failed AJAX:",error); $( "#span_for_output_to_user" ).html( "404 error - couldn't get subreddit data." )},
   })
   // console.log( redditJSON ); // for testing only; doesn't work with async set to true.
-}
-
-// Just for testing whether the JSON is available after doing an async $.ajax call. Turns out it is available - once the async call returns, it saves to var redditJSON. The example I saw in Stack Overflow insisted that you needed async: false, but that was probably for some other reason. Saving to a variable works fine.
-function log_redditJSON_to_console() {
-  console.log( redditJSON );
-}
-// Just for testing URLarray
-function log_URLarray_to_console() {
-  console.log( URLarray );
 }
 
 // Function to parse all of the URLs from the Reddit JSON and store the ones from imgur.com and gfycat.com
@@ -96,12 +87,25 @@ function parse_URLs() { // console.log( redditJSON ); // for testing only
 }
 
 // Function to grab an imgur page and display all images from it
-function get_parse_display_imgur_page() {
-  
+function get_parse_display_imgur_page( imgur_page_URL ) {
+  $.ajax ({
+    url: imgur_page_URL,
+    dataType:'html',
+    timeout: 30000,
+    success: function(data) {
+      imgur_page = data;
+      // console.log( data ); // for testing only
+      $( "#span_for_output_to_user" ).html( "Loaded." );
+      parse_URLs();
+      display_content();
+    }, 
+    error: function(error){console.log("AJAX error:",error); $( "#span_for_output_to_user" ).html( "Error - are you sure that's a subreddit?" )},
+    404: function(error){console.log("404 failed AJAX:",error); $( "#span_for_output_to_user" ).html( "404 error - couldn't get subreddit data." )},
+  })
 }
 
 // Function to grab a gfycat page and display the movie from it
-function get_parse_display_gfycat_page() {
+function get_parse_display_gfycat_page( gfycat_page_URL ) {
   
 }
 
@@ -110,7 +114,7 @@ current_picture_number = 0;
 function display_content() {
   // console.log( URLarray ); // just for testing
   $( "#span_for_image_number" ).html( "Image #" + current_picture_number );
-  currentURL = URLarray[ current_picture_number ]; // Just to save typing
+  var currentURL = URLarray[ current_picture_number ]; // Just to save typing
   
   if ( is_URL_an_image( currentURL ) ) {
 	// If the URL is directly to an image, just display that image:
@@ -124,12 +128,12 @@ function display_content() {
 	$( "#responsive_div_for_video" ).attr( "src", currentURL )
     $( "#container_div_for_video" ).show()
     $( "#div_for_image" ).hide()
-  } else if ( is_URL_to_imgur_page( currentURL ) ) {
+  } else if ( is_URL_a_imgur_page( currentURL ) ) {
     // If the URL is to an imgur page, parse it and display all of the images.
-	
-  } else if ( is_URL_to_gfycat_page( currentURL ) ) {
+	get_parse_display_imgur_page( currentURL );
+  } else if ( is_URL_a_gfycat_page( currentURL ) ) {
     // If the URL is to a gfycat page, parse it and display the movie
-	
+	get_parse_display_gfycat_page( currentURL );
   }
 }
 
@@ -149,7 +153,7 @@ function is_URL_a_movie( URL_to_check ) {
 }
 
 // Return true if the URL is to imgur.com but not directly to an image or webm:
-function is_URL_to_imgur_page( URL_to_check ) {
+function is_URL_a_imgur_page( URL_to_check ) {
   if (
     ( URL_to_check.search("imgur.com") != -1 )
     && ! is_URL_an_image(URL_to_check)
@@ -159,7 +163,7 @@ function is_URL_to_imgur_page( URL_to_check ) {
 }
 
 // Return true if the URL is to gfycat.com but not directly to a webm:
-function is_URL_to_gfycat_page( URL_to_check ) {
+function is_URL_a_gfycat_page( URL_to_check ) {
   if (
     ( URL_to_check.search("gfycat.com") != -1 )
     && ! is_URL_an_image(URL_to_check)
@@ -186,6 +190,15 @@ function back() {
     current_picture_number -= 1;
 	display_content();
   }
+}
+
+// Just for testing whether the JSON is available after doing an async $.ajax call. Turns out it is available - once the async call returns, it saves to var redditJSON. The example I saw in Stack Overflow insisted that you needed async: false, but that was probably for some other reason. Saving to a variable works fine.
+function log_redditJSON_to_console() {
+  console.log( redditJSON );
+}
+// Just for testing URLarray
+function log_URLarray_to_console() {
+  console.log( URLarray );
 }
 
 // --------------------------------------------------------------------------------------------------
