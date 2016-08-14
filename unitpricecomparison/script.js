0d5b6642b3a0114419e637a7ff0fdb55928522fb
Paul Koepke <paul.koepke@gmail.com>
2016-08-14 14:42:04 -0400

    UPC making transition 0.5s again.

--- unitpricecomparison/script.js
+++ unitpricecomparison/script.js
@@ -4,7 +4,7 @@ window.addEventListener('load', addIosStyles, false);
 window.addEventListener('load', addDesktopStyles, false);
 window.addEventListener('load', addTransitionSyles, false);
 
-var transitionDelayInSeconds = 1;
+var transitionDelayInSeconds = 0.5;
 var transitionDelayInMiliSeconds = transitionDelayInSeconds * 1000;
 var secondRowInitialHeight = '81px'; // just throwing in a default height, this should be overwritten when the page loads by storeSecondRowHeight().
 
