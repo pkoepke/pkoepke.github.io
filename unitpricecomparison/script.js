1c6b9eca450cfa27dd47c6d4367464c1dfd5d20f
Paul Koepke <paul.koepke@gmail.com>
2016-10-04 16:02:04 -0400

    UPC removing console.log.

--- unitpricecomparison/script.js
+++ unitpricecomparison/script.js
@@ -191,52 +191,13 @@ function addTransitionSyles() {
 }
 
 function addStyles() {
-  console.log(document.getElementById('stylesTag'));
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
   if( userAgent.indexOf('iPad') > -1 || userAgent.indexOf('iPhone') > -1 || userAgent.indexOf('iPod') > -1 ) {
     //document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="./iosStyles.css">';
-    document.getElementById('stylesTag').innerHTML += 'h3 {
-      font-size: 3rem;
-    }
-
-    button {
-      height: 1em;
-      min-width: 0;
-      max-width 2em;
-      vertical-align: 15%;
-    }
-
-    .itemLabel, .price, .units, .itemPricePerUnits, .itemName, .quantity, .unitsName {
-      width: 22%;
-    }
-';
+    document.getElementById('stylesTag').innerHTML += 'h3 {font-size: 3rem;} button {height: 1em; min-width: 0; max-width 2em; vertical-align: 15%;} .itemLabel, .price, .units, .itemPricePerUnits, .itemName, .quantity, .unitsName {width: 22%;}';
   } else if (!detectBobileBrowsers()) {
     //document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="./desktopStyles.css">';
-    document.getElementById('stylesTag').innerHTML += 'body {
-      max-width: 960px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-
-    h3 {
-      font-size: 2rem;
-    }
-
-    button {
-      height: 3rem;
-      min-width: 4rem;
-    }
-
-    .multiplicationSign, .expandCollapseArrow, .addRowImg, .removeRowImg {
-      height: 2rem;
-      vertical-align: 10%;
-    }
-
-
-    .itemLabel, .price, .units, .itemPricePerUnits, .itemName, .quantity, .unitsName {
-      font-size: 1.5rem;
-    }
-';
+    document.getElementById('stylesTag').innerHTML += 'body {max-width: 960px; margin-left: auto; margin-right: auto;} h3 {font-size: 2rem;} button {height: 3rem; min-width: 4rem;} .multiplicationSign, .expandCollapseArrow, .addRowImg, .removeRowImg {height: 2rem; vertical-align: 10%;} .itemLabel, .price, .units, .itemPricePerUnits, .itemName, .quantity, .unitsName {font-size: 1.5rem;}';
   }
 }
 
