adb2d127dbb909b83011f2bee47257c8ca3cef43
Paul Koepke <paul.koepke@gmail.com>
2016-10-04 15:28:20 -0400

    UPC - moving all static resources to cdn.rawgit.com.

--- unitpricecomparison/script.js
+++ unitpricecomparison/script.js
@@ -81,7 +81,7 @@ function highlightLowestPricePerUnit(allDataObject) {
     }
   });
   try { allDataObject.allItemPricePerUnitsSpans[lowestPpuObject.lowestIndex].className = 'itemPricePerUnits highlightedPricePerUnits transition';
-  } catch (err) { /* getElementById will be null if there is no unit prices to highlight, so errors are expected here. There's no need to do anything in resposne to the error. */ }
+  } catch (err) { /* The array item will be null if there is no unit prices to highlight, so errors are expected here. There's no need to do anything in resposne to the error. */ }
 }
 
 function clearAll() {
@@ -191,11 +191,52 @@ function addTransitionSyles() {
 }
 
 function addStyles() {
+  console.log(document.getElementById('stylesTag'));
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
   if( userAgent.indexOf('iPad') > -1 || userAgent.indexOf('iPhone') > -1 || userAgent.indexOf('iPod') > -1 ) {
-    document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="./iosStyles.css">';
+    //document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="./iosStyles.css">';
+    document.getElementById('stylesTag').innerHTML += 'h3 {
+      font-size: 3rem;
+    }
+
+    button {
+      height: 1em;
+      min-width: 0;
+      max-width 2em;
+      vertical-align: 15%;
+    }
+
+    .itemLabel, .price, .units, .itemPricePerUnits, .itemName, .quantity, .unitsName {
+      width: 22%;
+    }
+';
   } else if (!detectBobileBrowsers()) {
-    document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="./desktopStyles.css">';
+    //document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="./desktopStyles.css">';
+    document.getElementById('stylesTag').innerHTML += 'body {
+      max-width: 960px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    h3 {
+      font-size: 2rem;
+    }
+
+    button {
+      height: 3rem;
+      min-width: 4rem;
+    }
+
+    .multiplicationSign, .expandCollapseArrow, .addRowImg, .removeRowImg {
+      height: 2rem;
+      vertical-align: 10%;
+    }
+
+
+    .itemLabel, .price, .units, .itemPricePerUnits, .itemName, .quantity, .unitsName {
+      font-size: 1.5rem;
+    }
+';
   }
 }
 
