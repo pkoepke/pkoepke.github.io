4a5a8259822226bf131a5d8a0cf73a7f743260f3
Paul Koepke <paul.koepke@gmail.com>
2017-02-09 10:40:34 -0500

    Tip Calculator - using JS to make buttons slightly larger on iOS only.

--- tip_calculator/script_unminified.js
+++ tip_calculator/script_unminified.js
@@ -8,6 +8,7 @@
 // Add event listeners to the windows to run code when the page loads.
 window.addEventListener('load', displayOriginalOutput, false);
 window.addEventListener('load', giveBillAmountFocus, false);
+window.addEventListener('load', applyIosStyles, false);
 
 function giveBillAmountFocus(event) {
   document.getElementById('billAmount').focus();
@@ -30,44 +31,44 @@ function calculatePercentWithRounding(event) {
   var tipPercent = tipPercentInputElement.value;
 
   /*if (billAmount < 0) {
-    billAmountInputElement.value = billAmount * -1;
-    billAmount = billAmountInputElement.value;
+  billAmountInputElement.value = billAmount * -1;
+  billAmount = billAmountInputElement.value;
+}
+
+if (tipPercent < 0) {
+tipPercentInputElement.value = tipPercent * -1;
+tipPercent = tipPercentInputElement.value;
+}*/
+
+// validate the the inputs are numbers. If they aren't, store False in var areFieldsNumbers so we know to stop and show an error. If they are numbers, store True in var areFieldsNumbers so the computations continue.
+var areFieldsNumbers = checkInputsDisplayErrorsReturn(billAmount, tipPercent);
+
+// if both fields are numbers, calculate.
+if(areFieldsNumbers) {
+  // first do the calculations without rounding and output the result. These values will be used during the rounding calculations.
+  var tipAmountUnrounded = Number(billAmount * (tipPercent / 100));
+  var totalAmountUnrounded = Number(billAmount) + Number(tipAmountUnrounded);
+  document.getElementById('output').innerHTML = tipPercent + '% of $' + (Math.round(billAmount * 100) / 100).toFixed(2) + ' is $' + (Math.round(tipAmountUnrounded * 100) / 100).toFixed(2) + ', so your total should be $' + ((Math.round(totalAmountUnrounded * 100) / 100).toFixed(2)) + '.<br /><br />';
+
+  // second do the calculations without and withrounding using values from unrounded calculations
+  var totalAmountRoundedUp = Math.ceil(totalAmountUnrounded);
+  var tipAmountRoundingUp = Math.round((totalAmountRoundedUp - billAmount) * 100) / 100;
+  var tipPercentRoundingUp = Math.round((tipAmountRoundingUp / billAmount) * 100) / 100;
+
+  var totalAmountRoundedDown = Math.floor(totalAmountUnrounded);
+  var tipAmountRoundingDown = Math.round((totalAmountRoundedDown - billAmount) * 100) / 100;
+  var tipPercentRoundingDown = Math.round((tipAmountRoundingDown / billAmount) * 100) / 100;
+
+  // output rounded up result
+  document.getElementById('output').innerHTML += tipPercent + '% rounding up is a tip of $' + tipAmountRoundingUp.toFixed(2) + ', so your total should be $' + (Number(billAmount) + Number(tipAmountRoundingUp)).toFixed(2) + ' which is a tip of ' + Number(tipPercentRoundingUp * 100).toFixed(0) + '%.<br /><br />';
+
+  // rounding down results in negative tips for bills of $1.01-$1.66, $2.01-$2.49, $3.01-$3.33, or $4.01-4.16. We'll display a warning instead of giving the negative tip amount.
+  if(tipAmountRoundingDown >= 0) {
+    document.getElementById('output').innerHTML += tipPercent + '% rounding down is a tip of $' + tipAmountRoundingDown.toFixed(2) + ', so your total should be $' + (Number(billAmount) + Number(tipAmountRoundingDown)).toFixed(2) + ' which is a tip of ' + Number(tipPercentRoundingDown * 100).toFixed(0) + '%.';
+  } else {
+    document.getElementById('output').innerHTML += tipPercent + '% rounding down would be a negative or zero tip ($' + tipAmountRoundingDown.toFixed(2) + ') because the bill amount is very small.';
   }
-
-  if (tipPercent < 0) {
-    tipPercentInputElement.value = tipPercent * -1;
-    tipPercent = tipPercentInputElement.value;
-  }*/
-
-  // validate the the inputs are numbers. If they aren't, store False in var areFieldsNumbers so we know to stop and show an error. If they are numbers, store True in var areFieldsNumbers so the computations continue.
-  var areFieldsNumbers = checkInputsDisplayErrorsReturn(billAmount, tipPercent);
-
-  // if both fields are numbers, calculate.
-  if(areFieldsNumbers) {
-    // first do the calculations without rounding and output the result. These values will be used during the rounding calculations.
-    var tipAmountUnrounded = Number(billAmount * (tipPercent / 100));
-    var totalAmountUnrounded = Number(billAmount) + Number(tipAmountUnrounded);
-    document.getElementById('output').innerHTML = tipPercent + '% of $' + (Math.round(billAmount * 100) / 100).toFixed(2) + ' is $' + (Math.round(tipAmountUnrounded * 100) / 100).toFixed(2) + ', so your total should be $' + ((Math.round(totalAmountUnrounded * 100) / 100).toFixed(2)) + '.<br /><br />';
-
-    // second do the calculations without and withrounding using values from unrounded calculations
-    var totalAmountRoundedUp = Math.ceil(totalAmountUnrounded);
-    var tipAmountRoundingUp = Math.round((totalAmountRoundedUp - billAmount) * 100) / 100;
-    var tipPercentRoundingUp = Math.round((tipAmountRoundingUp / billAmount) * 100) / 100;
-
-    var totalAmountRoundedDown = Math.floor(totalAmountUnrounded);
-    var tipAmountRoundingDown = Math.round((totalAmountRoundedDown - billAmount) * 100) / 100;
-    var tipPercentRoundingDown = Math.round((tipAmountRoundingDown / billAmount) * 100) / 100;
-
-    // output rounded up result
-    document.getElementById('output').innerHTML += tipPercent + '% rounding up is a tip of $' + tipAmountRoundingUp.toFixed(2) + ', so your total should be $' + (Number(billAmount) + Number(tipAmountRoundingUp)).toFixed(2) + ' which is a tip of ' + Number(tipPercentRoundingUp * 100).toFixed(0) + '%.<br /><br />';
-
-    // rounding down results in negative tips for bills of $1.01-$1.66, $2.01-$2.49, $3.01-$3.33, or $4.01-4.16. We'll display a warning instead of giving the negative tip amount.
-    if(tipAmountRoundingDown >= 0) {
-      document.getElementById('output').innerHTML += tipPercent + '% rounding down is a tip of $' + tipAmountRoundingDown.toFixed(2) + ', so your total should be $' + (Number(billAmount) + Number(tipAmountRoundingDown)).toFixed(2) + ' which is a tip of ' + Number(tipPercentRoundingDown * 100).toFixed(0) + '%.';
-    } else {
-      document.getElementById('output').innerHTML += tipPercent + '% rounding down would be a negative or zero tip ($' + tipAmountRoundingDown.toFixed(2) + ') because the bill amount is very small.';
-    }
-  }
+}
 }
 
 function checkInputsDisplayErrorsReturn(billAmount, tipPercent) { // Checks whether the inputs are numbers, if not displays an error in the output and returns False so computations stop.
@@ -99,42 +100,47 @@ function displayOriginalOutput() {
 function isAllowedKey(event) { // combining several answers from http://stackoverflow.com/questions/2808184/restricting-input-to-textbox-allowing-only-numbers-and-decimal-point
   /*console.log('onkeydown: ')
   function logAllProperties(obj) {
-       if (obj == null) return; // recursive approach
-       console.log(Object.getOwnPropertyNames(obj));
-       logAllProperties(Object.getPrototypeOf(obj));
-  }
-  logAllProperties(event);*/
+  if (obj == null) return; // recursive approach
+  console.log(Object.getOwnPropertyNames(obj));
+  logAllProperties(Object.getPrototypeOf(obj));
+}
+logAllProperties(event);*/
 
-  var keyCode = event.keyCode; // grab they key code
-  var variousAllowedkeyCodes = [8, 9, 46, 35, 36, 37, 38, 39, 40, 115]; // always-allowed key codes. Allowed: backspace 8, tab 9, delete 46, end 35, home 36, left arrow 37, up arrow 38, right arrow 39, down arrow 40, F5 115.
-  if (variousAllowedkeyCodes.indexOf(keyCode) != -1) { // return true if an allowed control key is pressed.
-    return true;
-  } else if ((48 <= keyCode && keyCode <= 57) || (96 <= keyCode && keyCode <= 105)) { // return true if a keyboard number or numpad key is pressed
-    return true;
-  } else if (keyCode == 110 || keyCode == 190) {
-    // return true if the period or numpad decimal is pressed, but only if there is not already a decimal in the number. If there is already a decimal, return false so only one decimal can be entered.
-    // This DOES NOT WORK for numbers with trailing decimals like '1.'' because event.target.value for a numeric input does not return trailing decimals. So we can't detect extra decimals unless there is a character after the decimal. So '1.1.' or '.3.' can be prevented, but not '1..'.
-    // Also, once two decimals make it into the field, .value completely breaks because the value is NaN. So '1..1.' is allowed because the first two decimals break .value .
-    // This could be fixed by making the input's type something else, like text, but then smartphones wouldn't present the user with a nice keypad to enter their numbers. So that's a no-go.
-    var currentValue = event.target.value;
-    if (((keyCode == 110) || (keyCode == 190)) && currentValue.indexOf('.') != -1) {
-      return false;
-    } else { return true; } // if there isn't already a decimal, return true.
-  }
-  else { return false; } // if the key wasn't caught by any of the conditions above, it isn't an allowed key. Return false.
+var keyCode = event.keyCode; // grab they key code
+var variousAllowedkeyCodes = [8, 9, 46, 35, 36, 37, 38, 39, 40, 115]; // always-allowed key codes. Allowed: backspace 8, tab 9, delete 46, end 35, home 36, left arrow 37, up arrow 38, right arrow 39, down arrow 40, F5 115.
+if (variousAllowedkeyCodes.indexOf(keyCode) != -1) { // return true if an allowed control key is pressed.
+  return true;
+} else if ((48 <= keyCode && keyCode <= 57) || (96 <= keyCode && keyCode <= 105)) { // return true if a keyboard number or numpad key is pressed
+  return true;
+} else if (keyCode == 110 || keyCode == 190) {
+  // return true if the period or numpad decimal is pressed, but only if there is not already a decimal in the number. If there is already a decimal, return false so only one decimal can be entered.
+  // This DOES NOT WORK for numbers with trailing decimals like '1.'' because event.target.value for a numeric input does not return trailing decimals. So we can't detect extra decimals unless there is a character after the decimal. So '1.1.' or '.3.' can be prevented, but not '1..'.
+  // Also, once two decimals make it into the field, .value completely breaks because the value is NaN. So '1..1.' is allowed because the first two decimals break .value .
+  // This could be fixed by making the input's type something else, like text, but then smartphones wouldn't present the user with a nice keypad to enter their numbers. So that's a no-go.
+  var currentValue = event.target.value;
+  if (((keyCode == 110) || (keyCode == 190)) && currentValue.indexOf('.') != -1) {
+    return false;
+  } else { return true; } // if there isn't already a decimal, return true.
+}
+else { return false; } // if the key wasn't caught by any of the conditions above, it isn't an allowed key. Return false.
 }
 
 // Determine if the operating system is iOS.
 function isIos() {
+  let userAgent = navigator.userAgent || navigator.vendor || window.opera;
   if( userAgent.indexOf('iPad') > -1 || userAgent.indexOf('iPhone') > -1 || userAgent.indexOf('iPod') > -1 ) {
     return true;
-  } else { return false; } 
+  } else { return false; }
 }
 
-// If the OS is iOS, move the dollar sign span to make it line up better.
-function fixDollarSignSpacingOnIos() {
-  if(isIos) {
+// If the OS is iOS, move the dollar sign span to make it line up better, and make the text on the buttons slightly larger.
+function applyIosStyles() {
+  if(isIos()) {
     document.getElementById('dollarSignSpan').style.left = '0.7em';
+    let textInputs = document.querySelectorAll('input[type=button]');
+    for (currentButton of textInputs) {
+      currentButton.style.fontSize = '1.2em';
+    }
   }
 }
 
