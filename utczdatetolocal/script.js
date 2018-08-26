a3e4d9ef58f1f0b8db4054f9562ba411cacf7f40
Paul Koepke <paul.koepke@gmail.com>
2018-08-26 13:35:27 -0400

    Removing UTC Z date to local converter, which was never finished and is no longer needed.

--- utczdatetolocal/script.js
+++
@@ -1,6 +0,0 @@
-function calculate() {
-  const originalDatetimeString = document.getElementById('inputDatetime');
-  const outputDate = new Date(originalDatetimeString);
-
-  document.getElementById('output').innerHTML = outputDate.toLocaleDateString()
-}
