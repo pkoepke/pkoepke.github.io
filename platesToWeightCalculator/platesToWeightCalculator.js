fd716709a0f8b654246481f56f82e7b7300f0038
Paul Koepke <paul.koepke@gmail.com>
2023-01-21 14:24:24 -0600

    * Renamed barbell plates calculator to weight to plates. * Added plates to weight calculator.

---
+++ platesToWeightCalculator/platesToWeightCalculator.js
@@ -0,0 +1,20 @@
+function calculate() {
+  let weight = 0, newWeight = 0;
+  newWeight = parseInt(document.getElementById('45s').value) * 45;
+  if (!isNaN(newWeight)) weight += newWeight;
+  newWeight = parseInt(document.getElementById('35s').value) * 35;
+  if (!isNaN(newWeight)) weight += newWeight;
+  newWeight = parseInt(document.getElementById('25s').value) * 25;
+  if (!isNaN(newWeight)) weight += newWeight;
+  newWeight = parseInt(document.getElementById('15s').value) * 15;
+  if (!isNaN(newWeight)) weight += newWeight;
+  newWeight = parseInt(document.getElementById('10s').value) * 10;
+  if (!isNaN(newWeight)) weight += newWeight;
+  newWeight = parseInt(document.getElementById('5s').value) * 5;
+  if (!isNaN(newWeight)) weight += newWeight;
+  newWeight = parseInt(document.getElementById('2.5s').value) * 2.5;
+  if (!isNaN(newWeight)) weight += newWeight;
+  newWeight = parseInt(document.getElementById('barWeight').value);
+  if (!isNaN(newWeight)) weight += newWeight;
+  document.getElementById('output').innerHTML = weight + " lbs";
+}
