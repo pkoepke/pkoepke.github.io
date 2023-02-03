bb89d3953941fbb5c19767e6097ba0b49a818e3a
Paul Koepke <paul.koepke@gmail.com>
2023-02-03 09:55:59 -0600

    Adding second Nitter instance to Twitter-Nitter URL converter.

--- twitternitter/twitternitter.js
+++ twitternitter/twitternitter.js
@@ -5,13 +5,19 @@ function twitterToNitter() {
   if (url.indexOf('http') != 0) {
     url = 'https://' + url;
   }
-  document.getElementById('twitterToNitterOutput').innerHTML = '<a href="' + url + '">' + url + '</a>';
+  url2 = url.replace('nitter.net', 'nitter.lacontrevoie.fr')
+  if (url2.indexOf('http') != 0) {
+    url2 = 'https://' + url2;
+  }
+  document.getElementById('twitterToNitterOutput').innerHTML = '<a href="' + url + '">' + url + '</a><br> <a href = "' + url2 + '" > ' + url2 + '</a >';
 }
 
-function nitterToTwitter() {  let url = document.getElementById('nitterUrl').value;
+function nitterToTwitter() {
+  let url = document.getElementById('nitterUrl').value;
   url = url.toLowerCase()
-  url = url.replace('nitter.net','twitter.com');
+  url = url.replace('nitter.net', 'twitter.com');
   if (url.indexOf('http') != 0) {
     url = 'https://' + url;
   }
-  document.getElementById('nitterToTwitterOutput').innerHTML = '<a href="' + url + '">' + url + '</a>';}
+  document.getElementById('nitterToTwitterOutput').innerHTML = '<a href="' + url + '">' + url + '</a>';
+}
