5151ceda044414da90680a29ed2c18a9c76badbc
Paul Koepke <paul.koepke@gmail.com>
2023-03-20 10:53:31 -0500

    Twitter Nitter converter: adding another domain and replacing <br> with <p>

--- twitternitter/twitternitter.js
+++ twitternitter/twitternitter.js
@@ -1,15 +1,20 @@
 function twitterToNitter() {
-  let url = document.getElementById('twitterUrl').value;
-  url = url.toLowerCase()
-  url = url.replace('twitter.com', 'nitter.net');
-  if (url.indexOf('http') != 0) {
-    url = 'https://' + url;
+  let ogUrl = document.getElementById('twitterUrl').value;
+  ogUrl = ogUrl.toLowerCase()
+  url1 = ogUrl.replace('twitter.com', 'nitter.net');
+  console.log(ogUrl)
+  if (url1.indexOf('http') != 0) {
+    url1 = 'https://' + url1;
   }
-  url2 = url.replace('nitter.net', 'nitter.lacontrevoie.fr')
+  url2 = ogUrl.replace('twitter.com', 'nitter.lacontrevoie.fr')
   if (url2.indexOf('http') != 0) {
     url2 = 'https://' + url2;
   }
-  document.getElementById('twitterToNitterOutput').innerHTML = '<a href="' + url + '">' + url + '</a><br> <a href = "' + url2 + '" > ' + url2 + '</a >';
+  url3 = ogUrl.replace('twitter.com', 'nitter.1d4.us')
+  if (url3.indexOf('http') != 0) {
+    url3 = 'https://' + url3;
+  }
+  document.getElementById('twitterToNitterOutput').innerHTML = '<p><a href="' + url1 + '">' + url1 + '</a></p><p><a href="' + url2 + '" > ' + url2 + '</a ></p><p><a href="' + url3 + '" > ' + url3 + '</a ></p>';
 }
 
 function nitterToTwitter() {
