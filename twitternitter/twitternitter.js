1efd316fb64b90e67e24c73cf4fd962515c8dcbc
Paul Koepke <paul.koepke@gmail.com>
2023-04-10 07:37:42 -0500

    Twitter Nitter converter: made it a single input that generates all outputs simultaneously.

--- twitternitter/twitternitter.js
+++ twitternitter/twitternitter.js
@@ -1,28 +1,12 @@
-function twitterToNitter() {
-  let ogUrl = document.getElementById('twitterUrl').value;
-  ogUrl = ogUrl.toLowerCase()
-  url1 = ogUrl.replace('twitter.com', 'nitter.net');
-  console.log(ogUrl)
-  if (url1.indexOf('http') != 0) {
-    url1 = 'https://' + url1;
+function convertAll() {
+  document.getElementById('output').innerHTML = '';
+  let url = new URL(document.getElementById('url').value);
+  let domains = ['www.twitter.com', 'www.nitter.1d4.us', 'www.nitter.net', 'www.nitter.lacontrevoie.fr']
+  let results = []
+  for (domain of domains) {
+    results.push('<a href="https://' + domain + url.pathname + '">' + domain + url.pathname + '</a>')
   }
-  url2 = ogUrl.replace('twitter.com', 'nitter.lacontrevoie.fr')
-  if (url2.indexOf('http') != 0) {
-    url2 = 'https://' + url2;
+  for (result of results) {
+    document.getElementById('output').innerHTML += '<p>' + result + '</p>';
   }
-  url3 = ogUrl.replace('twitter.com', 'nitter.1d4.us')
-  if (url3.indexOf('http') != 0) {
-    url3 = 'https://' + url3;
-  }
-  document.getElementById('twitterToNitterOutput').innerHTML = '<p><a href="' + url1 + '">' + url1 + '</a></p><p><a href="' + url2 + '" > ' + url2 + '</a ></p><p><a href="' + url3 + '" > ' + url3 + '</a ></p>';
-}
-
-function nitterToTwitter() {
-  let url = document.getElementById('nitterUrl').value;
-  url = url.toLowerCase()
-  url = url.replace('nitter.net', 'twitter.com');
-  if (url.indexOf('http') != 0) {
-    url = 'https://' + url;
-  }
-  document.getElementById('nitterToTwitterOutput').innerHTML = '<a href="' + url + '">' + url + '</a>';
 }
