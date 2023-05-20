82d109e3dd9d4d8f8b95da9947bf00b960a5fdca
Paul Koepke <paul.koepke@gmail.com>
2023-05-20 08:57:07 -0500

    MF:  * added tags to index.html  * fixed script link to service worker.

--- miniflux_offline_client/script.js
+++ miniflux_offline_client/script.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(
+  navigator.serviceWorker.register("serviceworker.js").then(
     (registration) => {
       console.log("Service worker registration succeeded:", registration);
     },
