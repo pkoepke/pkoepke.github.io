fe183d66b67d1f046544d18a1f68a6a3030994b9
Paul Koepke <paul.koepke@gmail.com>
2023-05-20 09:13:50 -0500

    MF: starting on SW caching.

--- miniflux_offline_client/serviceworker.js
+++ miniflux_offline_client/serviceworker.js
@@ -1,1 +1,12 @@
-console.log('the Service Worker did a console.log!')
+// Once the SW is installed, cache files for offline use.
+self.addEventListener("install", (e) => {
+  console.log("Service Worker Install event fired, caching files.");
+  const contentToCache = ['index.html', 'script.js', 'styles.css', '/favicon.png']; // Array of URLs to cache.
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })()
+  );
+});
