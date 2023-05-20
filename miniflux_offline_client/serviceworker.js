008a91d063623947c013a1dbd98afc7b6e977992
Paul Koepke <paul.koepke@gmail.com>
2023-05-20 09:26:12 -0500

    MF added serving resources form cache where possible.

--- miniflux_offline_client/serviceworker.js
+++ miniflux_offline_client/serviceworker.js
@@ -9,13 +9,25 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
-      console.log("[Service Worker] Caching all: app shell and content");
       await cache.addAll(contentToCache);
     })()
   );
 });
 
-// Listen for network requests.
+// Listen for network requests and server from cache if possible.
 self.addEventListener("fetch", (e) => {
-  console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
 });
