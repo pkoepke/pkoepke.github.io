307a427b5fbeb6943f2d2a3622a9894fc83f7959
Paul Koepke <paul.koepke@gmail.com>
2023-05-20 09:39:50 -0500

    MF trying to update SW on every load for testing purposes.

--- miniflux_offline_client/script.js
+++ miniflux_offline_client/script.js
@@ -2,6 +2,7 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("serviceworker.js").then(
     (registration) => {
       console.log("Service worker registration succeeded:", registration);
+      registration.update(); // Go ahead and update the SW every time while testing.
     },
     (error) => {
       console.error(`Service worker registration failed: ${error}`);
