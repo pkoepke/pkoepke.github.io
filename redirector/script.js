c1087152485677e9baaf5c1e962e74dc796ccf50
Paul Koepke <paul.koepke@gmail.com>
2023-05-26 06:19:10 -0500

    redirect

--- redirector/script.js
+++ redirector/script.js
@@ -5,7 +5,7 @@ const redirect = () => {
   if ((newUrl.indexOf('extrafabulouscomics.com') >= 0) && ((newUrl.indexOf('extrafabulouscomics.com') <= 12))) {
     window.location.replace(newUrl);
   } else {
-    document.body.innerHTML += "Invalid domain."
+    document.body.innerHTML += '<p>Invalid domain.</p>';
   }
 }
 redirect();
