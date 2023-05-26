fdb7a1d35dd7ae8734c741116ed1c3cd5d842efa
Paul Koepke <paul.koepke@gmail.com>
2023-05-26 06:11:32 -0500

    Redirector might be working now.

--- redirector/script.js
+++ redirector/script.js
@@ -1,11 +1,11 @@
-console.log('script ran')
-
 const redirect = () => {
   newUrl = new URLSearchParams(window.location.search).get('redirectUrl');
   console.log(newUrl);
   console.log(newUrl.indexOf('extrafabulouscomics.com'));
-  if ((newUrl.indexOf('extrafabulouscomics.com') > -1) && ((newUrl.indexOf('extrafabulouscomics.com') > 12))) {
-    window.location.href = newUrl;
+  if ((newUrl.indexOf('extrafabulouscomics.com') >= 0) && ((newUrl.indexOf('extrafabulouscomics.com') <= 12))) {
+    window.location.replace(newUrl);
+  } else {
+    document.body.innerHTML += "Invalid domain."
   }
 }
 redirect();
