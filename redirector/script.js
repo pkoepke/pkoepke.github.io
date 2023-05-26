6547934051b8bf69425407ca163cda7b5c0d018c
Paul Koepke <paul.koepke@gmail.com>
2023-05-26 08:45:48 -0500

    Redirector

--- redirector/script.js
+++ redirector/script.js
@@ -1,7 +1,21 @@
-const redirect = () => {
+const calculateCurrentPrevNext = (currentComic) => {
+  currentComic = currentComic.slice(-5); // example: https://www.extrafabulouscomics.com/____1
+  currentComic = parseInt(currentComic.replace(/\D/g, ''));
+
+  let nextComic = currentComic + 1;
+  nextComic = nextComic.toString().padStart(5, '_');
+  nextComic = 'https://www.extrafabulouscomics.com/' + nextComic;
+
+  let prevComic = currentComic - 1;
+  prevComic = prevComic.toString().padStart(5, '_');
+  prevComic = 'https://www.extrafabulouscomics.com/' + prevComic;
+
+  return { 'current': currentComic, 'next': nextComic, 'prev': prevComic };
+}
+
+const redirect = (direction) => {
   newUrl = new URL(new URLSearchParams(window.location.search).get('redirectUrl'));
-  console.log(newUrl);
-  console.log(newUrl.hostname);
+  newUrl = calculateCurrentPrevNext(newUrl.toString());
 
   if (newUrl.hostname == 'extrafabulouscomics.com' || newUrl.hostname == 'www.extrafabulouscomics.com') {
     window.location.replace(newUrl);
