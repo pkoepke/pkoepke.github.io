7e5452be71849e773067ff394f5fa13b7896e2d6
Paul Koepke <paul.koepke@gmail.com>
2023-05-26 06:06:38 -0500

    redirector updates.

--- redirector/script.js
+++ redirector/script.js
@@ -1,6 +1,11 @@
 console.log('script ran')
 
 const redirect = () => {
-  console.log(window.location.href)
+  newUrl = new URLSearchParams(window.location.search).get('redirectUrl');
+  console.log(newUrl);
+  console.log(newUrl.indexOf('extrafabulouscomics.com'));
+  if ((newUrl.indexOf('extrafabulouscomics.com') > -1) && ((newUrl.indexOf('extrafabulouscomics.com') > 12))) {
+    window.location.href = newUrl;
+  }
 }
 redirect();
