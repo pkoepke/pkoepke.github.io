26fd3fbcf1e98f80a584787315a40d144c2edffd
Paul Koepke <paul.koepke@gmail.com>
2023-07-11 15:18:22 -0500

    TwitterNitter try catch

--- twitternitter/twitternitter.js
+++ twitternitter/twitternitter.js
@@ -1,14 +1,18 @@
 function convertAll() {
+  try {
+    let url = new URL(document.getElementById('url').value);
+    let domains = ['www.twitter.com', 'nitter.1d4.us', 'nitter.net', 'nitter.lacontrevoie.fr']
+    let results = []
+    for (domain of domains) {
+      results.push('<a href="https://' + domain + url.pathname + '">' + domain + url.pathname + '</a>')
+    }
+    for (result of results) {
+      document.getElementById('output').innerHTML += '<p>' + result + '</p>';
+    }
+  } catch (error) { // If the URL is invalid, do nothing except clearing the output.
+  }
   document.getElementById('output').innerHTML = '';
-  let url = new URL(document.getElementById('url').value);
-  let domains = ['www.twitter.com', 'nitter.1d4.us', 'nitter.net', 'nitter.lacontrevoie.fr']
-  let results = []
-  for (domain of domains) {
-    results.push('<a href="https://' + domain + url.pathname + '">' + domain + url.pathname + '</a>')
-  }
-  for (result of results) {
-    document.getElementById('output').innerHTML += '<p>' + result + '</p>';
-  }
+  
 }
 
 function paste() {
