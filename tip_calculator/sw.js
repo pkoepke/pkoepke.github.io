97cd89555dac4857e756a39fa52dbf15e9a66aa7
Paul Koepke <paul.koepke@gmail.com>
2024-02-10 16:37:11 -0600

    * Serving many pages from root /sw.js and default shared cache * Making separate caches for PWAs whose Service Workers should work alone.

--- tip_calculator/sw.js
+++ tip_calculator/sw.js
@@ -1,6 +1,6 @@
 // This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline";
+const CACHE = "tip-calculator-cache";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
