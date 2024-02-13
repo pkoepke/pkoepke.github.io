8106a4dc3b503d97e496aae36673982b8f2d01e9
Paul Koepke <paul.koepke@gmail.com>
2024-02-13 16:27:39 -0600

    Root service worker: adding precaching of key pages.

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-// This is the "Offline copy of pages" service worker
-
 const CACHE = "root-cache";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
@@ -8,6 +6,39 @@ self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
+});
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open("CACHE").then((cache) => {
+    cache.addAll([
+      '/', // Begin main page
+      '/index.html',
+      '/styles_dark.css',
+      '/noto_sans/NotoSans-Regular.ttf',
+      '/favicon.svg',
+      '/tip_calculator/', // Begin Tip Calculator
+      '/tip_calculator/index.html',
+      '/tip_calculator/styles.css',
+      '/tip_calculator/darkTheme.css',
+      '/tip_calculator/script.js',
+      '/tip_calculator/tipCalculatorIcon_128x128.png',
+      '/redditSearch/', // Begin Reddit Search Generator
+      '/redditSearch/index.html',
+      '/redditSearch/redditSearch.js',
+      '/twitternitter/', // Begin twitternitter
+      '/twitternitter/index.html',
+      '/twitternitter/styles.css',
+      '/twitternitter/twitternitter.js',
+      '/bettingOddsTranslator/', // Begin betting odds
+      '/bettingOddsTranslator/index.html',
+      '/bettingOddsTranslator/bettingOddsTranslator.js',
+      'platesAndWeight/', // Begin plates and weights
+      'platesAndWeight/index.html',
+      'platesAndWeight/styles.css',
+      'platesAndWeight/script.js',
+      'platesAndWeight/barbell%20512x512.png'
+    ]);
+  },),);
 });
 
 workbox.routing.registerRoute(
