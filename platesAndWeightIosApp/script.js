719ce5e86030ddffa84d3b9defbabe9485408cb6
Paul Koepke <paul.koepke@gmail.com>
2024-02-20 20:45:48 -0600

    Weights and plates: helper function for input restructions. Not used yet.
    WaP iOS: tons of updates, all buttons work now but need more testing.

--- platesAndWeightIosApp/script.js
+++ platesAndWeightIosApp/script.js
@@ -1,18 +1,21 @@
 // Weight from plates functions.
 
+let platesLabels = [];
 
-const isInteger = (event) => {
+const getPlatesLabels = () => {
+  platesLabels = [...document.getElementsByClassName('labelForPlates')];
+}
+
+/*const isInteger = (event) => {
   if (!Number.isInteger((parseFloat(event.data)))) {
     event.preventDefault();
   }
-}
-
+}*/
 
 function calculateWeightFromPlates() {
   document.getElementById('desiredWeightLabel').innerHTML = 'Weight:' // If the user is entering plates, change the label to Weight to make it clearer we're going from plates to weight.
   clearRemainingWeight();
   let weight = 0, newWeight = 0; // newWeight is the weight to add at each step. It's needed to check for NaN results.
-  const platesLabels = [...document.getElementsByClassName('labelForPlates')]
   for (const label of platesLabels) {
     let labelNum = parseInt(label.innerText);
     if (label.innerText == '2.5s:') labelNum = 2.5; // 2.5 is a float and needs special handling
@@ -20,7 +23,7 @@ function calculateWeightFromPlates() {
     newWeight = numPlates * labelNum;
     if (!isNaN(newWeight)) weight += newWeight;
   }
-  newWeight = parseInt(document.getElementById('barWeight').value);
+  newWeight = parseInt(document.getElementById('bar').value);
   if (!isNaN(newWeight)) weight += newWeight;
   document.getElementById('desiredWeight').value = weight;
   document.getElementById('percent').value = Math.round(100 * (document.getElementById('desiredWeight').value) / parseInt(document.getElementById('1RM').value));// Recalculate % of 1RM.
@@ -53,14 +56,14 @@ const calculatePlates = () => {
   document.getElementById('desiredWeightLabel').innerHTML = 'Desired weight:' // If the user is entering a weight, change the label to Desired Weight to make it clear we're going from weight to plates.
 
   const desiredWeight = parseFloat(document.getElementById('desiredWeight').value);
-  const barWeight = parseFloat(document.getElementById('barWeight').value);
+  const bar = parseFloat(document.getElementById('bar').value);
   const fifteensYesNo = document.getElementById('fifteensYesNo').checked;
 
   if (isNaN(desiredWeight)) { // Can't do anything with this, just exit the function.
     clearRemainingWeight();
     return;
   }
-  if (desiredWeight < barWeight) { // Can't have a desired weight less than the bar, so just make all the # of plates zero then exit the function.
+  if (desiredWeight < bar) { // Can't have a desired weight less than the bar, so just make all the # of plates zero then exit the function.
     clearRemainingWeight();
     const plateInputs = [...document.getElementsByClassName('plateInput')];
     for (const input of plateInputs) {
@@ -68,7 +71,7 @@ const calculatePlates = () => {
     }
     return;
   } else {
-    let remainingWeight = desiredWeight - barWeight;
+    let remainingWeight = desiredWeight - bar;
     const a45s = parseInt(remainingWeight / 90) * 2;
     remainingWeight = remainingWeight - (a45s * 45);
     const a35s = parseInt(remainingWeight / 70) * 2;
@@ -93,7 +96,7 @@ const calculatePlates = () => {
       clearRemainingWeight();
     }
 
-    let output = barWeight + 'lbs bar<br>'
+    let output = bar + 'lbs bar<br>'
     if (a45s) {
       output += a45s + ' 45s, ' + a45s / 2 + ' per side<br>';
       document.getElementById('45s').value = a45s;
@@ -137,6 +140,103 @@ function clearRemainingWeight() {
   document.getElementById('remainingWeight').innerHTML = '';
 }
 
+const getLabels = () => {
+  let labelsToSave = [];
+  let labelElements = [...document.querySelectorAll('.labelForPlates'), ...document.querySelectorAll('.labelForWeight')];
+  for (element of labelElements) {
+    labelsToSave.push(element.getAttribute('for'));
+  }
+  return labelsToSave;
+}
+
+const save = () => {
+  clearTestKey();
+  let objectToSave = {};
+  let labelsToSave = getLabels();
+  for (let label of labelsToSave) {
+    objectToSave[label] = document.getElementById(label).value;
+  }
+  localStorage.setItem(localStorage.length, JSON.stringify(objectToSave));
+}
+
+const clearTestKey = () => {
+  localStorage.removeItem('test key'); // Firefox creates an un wanted 'test key' on page reload. If it's in localstorage this removes it, and if it's not there this does nothing.
+}
+
+const loadPrevious = () => {
+  clearTestKey();
+  document.getElementById('savedData').innerHTML = '';
+  let keys = Object.keys(localStorage);
+  keys.sort(); // Object.keys(localStorage) doesn't always return the keys in order, so this ensures they are added to the page in order.
+  for (let key of keys) {
+    let dataObject = JSON.parse(localStorage.getItem(key));
+    let rowDiv = document.createElement('div');
+    rowDiv.className = 'outputRow';
+    let innerKeys = Object.keys(dataObject);
+    let rowNum = document.createElement('span');
+    rowNum.className = 'savedItem';
+    rowNum.textContent = key;
+    rowDiv.appendChild(rowNum);
+    let rowChildren = [];
+    for (let innerKey of innerKeys) {
+      let itemDiv = document.createElement('span');
+      itemDiv.className = 'savedItem ' + innerKey;
+      itemDiv.setAttribute('data-label', innerKey);
+      itemDiv.setAttribute('data-value', dataObject[innerKey]);
+      itemDiv.textContent = innerKey + ': ' + dataObject[innerKey] + ' ';
+      if (itemDiv.className.includes('Weight')) {
+        itemDiv.style.width = '9rem';
+        itemDiv.textContent = 'Weight: ' + dataObject[innerKey] + ' ';
+      } else if (itemDiv.className.includes('1RM') | itemDiv.className.includes('percent')) {
+        itemDiv.style.width = '7rem';
+      } else { itemDiv.style.width = '4rem'; }
+      rowChildren.push(itemDiv);
+    }
+    //rowChildren.splice(6, 0, rowChildren.splice(8, 1)[0]);
+    for (let child of rowChildren) {
+      rowDiv.appendChild(child);
+    }
+    let loadButton = document.createElement('input');
+    loadButton.setAttribute('type', 'button');
+    loadButton.setAttribute('value', 'Load');
+    loadButton.setAttribute('onclick', 'loadOne(event)');
+    loadButton.setAttribute('data-object-key', key);
+    rowDiv.appendChild(loadButton);
+    document.getElementById('savedData').appendChild(rowDiv);
+  }
+}
+
+const loadOne = (event) => {
+  let allLabels = getLabels();
+  for (let label of allLabels) {
+    let sourceElement = event.target.parentNode.getElementsByClassName(label)[0];
+    document.getElementById(label).value = sourceElement.getAttribute('data-value');
+  }
+}
+
+const clearStorage = () => { localStorage.clear() }
+
+const share = async () => {
+  let allLabels = getLabels();
+  let textToShare = '';
+  for (let label of allLabels) {
+    textToShare += label + ' ' + document.getElementById(label).value + '\n';
+  }
+  console.log(textToShare);
+  if (navigator.share) {
+    navigator.share({
+      title: 'Plates and Weight',
+      text: textToShare,
+      url: 'https://paulkoepke.com/',
+    })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+  }
+
+}
+
+document.addEventListener("DOMContentLoaded", clearTestKey); // Clear unwanted test key Firefox sometimes creates on refresh.
+document.addEventListener("DOMContentLoaded", getPlatesLabels);
 /*document.addEventListener("DOMContentLoaded", () => {
   for (element of document.querySelectorAll("input[type=number]")) {
     element.addEventListener("beforeinput", isInteger);
