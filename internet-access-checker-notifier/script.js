931890eae2789e80db4a6b3e6c284234724507e5
Paul Koepke <paul.koepke@gmail.com>
2024-08-06 15:53:10 -0500

    IC: removing delete all caches

--- internet-access-checker-notifier/script.js
+++ internet-access-checker-notifier/script.js
@@ -78,14 +78,6 @@ const runTests = async () => {
   if (shouldNotify) notify(`Internet access check finished.`, ``);
 }
 
-const deleteAllCaches = () => {
-  caches.keys().then(function (names) {
-    for (let name of names)
-      caches.delete(name);
-  });
-}
-
 document.addEventListener(`DOMContentLoaded`, () => {
   document.getElementById(`button`).addEventListener(`click`, runTests);
-  document.getElementById(`clearCache`).addEventListener(`click`, deleteAllCaches);
 });
