8fc1d24e2351d841002fe9156656c0b1619c73e1
Paul Koepke <paul.koepke@gmail.com>
2024-09-22 14:55:10 -0500

    Internet Access Checker: simplifying Service Worker code.

--- internet-access-checker-notifier/sw.js
+++ internet-access-checker-notifier/sw.js
@@ -2,21 +2,16 @@
 
 const CACHE = `internet-access-checker-and-notifier`;
 
-importScripts(`https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js`);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener(`message`, (event) => {
-  if (event.data && event.data.type === `SKIP_WAITING`) {
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
 
-workbox.routing.registerRoute(({ url, request, event }) => {
-  console.log(`workbox URL: ${url}`);
-  if (url.includes(`index.html`) || url.includes(`script.js`) || url.includes(`styles.css`) || url.includes(`signal-font-awesome`)) {
-    return true
-  } else return false
-},
+workbox.routing.registerRoute(
+  new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
-);
