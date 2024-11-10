359761c037226ab75afd33d8153ab3b61a705121
Paul Koepke <paul.koepke@gmail.com>
2024-11-10 16:15:48 -0600

    Filename fixer: adding share intent and favicon

--- filename-fixer/script.js
+++ filename-fixer/script.js
@@ -39,3 +39,11 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('toLowercase').addEventListener('click', toLowercase);
 
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const parsedUrl = new URL(window.location);
+  // searchParams.get() will properly handle decoding the values.
+  console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+  console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+  console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+});
