76b2aeed397f280a4b167e7817f80d4a2ff0c417
Paul Koepke <paul.koepke@gmail.com>
2026-05-05 19:11:04 -0500

    Internet access checker: adding font picker, removing notify for now.

--- internet-access-checker-notifier/script.js
+++ internet-access-checker-notifier/script.js
@@ -21,8 +21,8 @@ const notify = (title, body) => {
   navigator.serviceWorker.ready.then(function (registration) {
     registration.showNotification(title, {
       body: body,
-      icon: `signal-font-awesome 512x512.png`,
-      badge: `signal-font-awesome 512x512.png`
+      icon: `./material-symbols--signal-cellular-alt.png`,
+      badge: `./material-symbols--signal-cellular-alt.png`
     });
   });
 }
@@ -51,62 +51,103 @@ const calculateTimeAndRate = (size, startTime, endTime) => { // Return the time 
 }
 
 const runTests = async () => {
-  let shouldNotify = document.getElementById(`notify`).checked;
-  if (shouldNotify) await askNotificationPermission();
+  const shouldNotifySuccess = document.getElementById(`notifySuccess`).checked;
+  const shouldNotifyFailure = document.getElementById(`notifyFailure`).checked;
+  if (shouldNotifySuccess || shouldNotifyFailure) await askNotificationPermission();
   let outputText = ``;
   const filesToCheck = [`1 kbit.zip`, `1 kbyte.bin`, `10 kbits.zip`, `10 kbytes.bin`, `100 kbits.zip`, `100 kbytes.bin`, `1 mbit.zip`, `1 mbyte.bin`, `10 mbits.zip`, `10 mbytes.bin`];
-  for (const path of filesToCheck) {
+  for (let path of filesToCheck) {
+    if (document.getElementById(`forceFailure`).checked) { path = `./nonexistentURLfor404`; }
     let outputElement = document.createElement(`div`);
     outputElement.textContent = `Trying ${path.split(`.`, 1)[0]}...`;
     document.getElementById(`output`).appendChild(outputElement);
     const startTime = new Date();
-    try {
-      const response = await fetch(`test-files/${path}`, { cache: "no-store" })
-      await response.text(); // Necessary so the script waits until the entire response has been received, not just started.
-      if (response.ok) {
-        const endTime = new Date();
-        const timeAndRate = calculateTimeAndRate(path, startTime, endTime);
-        //outputText = `${path.split(`.`, 1)[0]} succeeded in ${timeAndRate['seconds']} seconds for an effective rate of ${timeAndRate['rate']}.`;
-        const wrapperSpan = document.createElement(`span`);
-        wrapperSpan.classList.add(`flexRow`);
-        const firstSpan = document.createElement(`span`);
-        firstSpan.textContent = `${path.split(`.`, 1)[0]}`;
-        //firstSpan.style.width = `18%`;
-        firstSpan.style.width = `5rem`;
-        firstSpan.style.textAlign = `left`;
-        const secondSpan = document.createElement(`span`);
-        secondSpan.textContent = `${timeAndRate['seconds']} secs`;
-        //secondSpan.style.width = `34%`;
-        secondSpan.style.width = `5.5rem`;
-        secondSpan.style.textAlign = `right`;
-        const thirdSpan = document.createElement(`span`);
-        thirdSpan.textContent = `@`;
-        thirdSpan.style.width = `1.5rem`;
-        thirdSpan.style.textAlign = `center`;
-        const fourthSpan = document.createElement(`span`);
-        fourthSpan.textContent = `${timeAndRate['rate']}`
-        //fourthSpan.style.width = `47%`;
-        fourthSpan.style.width = `6.5rem`;
-        fourthSpan.style.textAlign = `right`;
-        wrapperSpan.appendChild(firstSpan);
-        wrapperSpan.appendChild(secondSpan);
-        wrapperSpan.appendChild(thirdSpan);
-        wrapperSpan.appendChild(fourthSpan);
-        document.getElementById(`output`).appendChild(wrapperSpan);
-      } else {
-        outputText = `${path.split(`.`, 1)[0]} failed.`;
-      }
-    } catch (e) {
-      outputText = `${path.split(`.`, 1)[0]} failed.`;
-    }
+    await doOneTest(path, startTime);
     outputElement.textContent = outputText;
-    if (shouldNotify) notify(outputText);
+    if (shouldNotifyFailure) notify(outputText); // Any response except response.ok is a failure.
   }
-  if (shouldNotify) notify(`Internet access check finished.`, ``);
+  const wrapperSpan = document.createElement(`span`);
+  wrapperSpan.classList.add(`flexRow`);
+  wrapperSpan.textContent += `Tests finished.`;
+  document.getElementById(`output`).appendChild(wrapperSpan);
+  if (shouldNotifySuccess || shouldNotifyFailure) notify(`Internet access check finished.`, ``);
 }
+
+const doOneTest = async (path, startTime) => {
+  let outputText = ``;
+  let response = ``;
+  try { response = await fetch(`test-files/${path}`, { cache: "no-store" }) }
+  catch (e) {
+    console.log(`Error: ${e}`)
+    handleFailure(path, startTime);
+  }
+  await response.text(); // Necessary so the script waits until the entire response has been received, not just started.
+  if (response.ok) {
+    document.getElementById(`output`).appendChild(handleSuccess(path, startTime));
+  } else {
+    console.log(`path: ${path}, startTime: ${startTime}`)
+    console.log(`document.getElementById(\`output\`): ${document.getElementById(`output`)}`)
+    document.getElementById(`output`).appendChild(handleFailure(path, startTime));
+  }
+  return outputText;
+}
+
+const handleFailure = (path, startTime) => {
+  outputText = `${path.split(`.`, 1)[0]} failed.`;
+  if (document.getElementById(`retryFailure`).checked) {
+    setTimeout(60000, async () => {
+      await doOneTest(path, startTime);;
+    });
+  }
+}
+
+const handleSuccess = (path, startTime) => {
+  const endTime = new Date();
+  const timeAndRate = calculateTimeAndRate(path, startTime, endTime);
+  //outputText = `${path.split(`.`, 1)[0]} succeeded in ${timeAndRate['seconds']} seconds for an effective rate of ${timeAndRate['rate']}.`;
+  const wrapperSpan = document.createElement(`span`);
+  wrapperSpan.classList.add(`flexRow`);
+  const firstSpan = document.createElement(`span`);
+  firstSpan.textContent = `${path.split(`.`, 1)[0]}`;
+  //firstSpan.style.width = `18%`;
+  firstSpan.style.width = `5rem`;
+  firstSpan.style.textAlign = `left`;
+  const secondSpan = document.createElement(`span`);
+  secondSpan.textContent = `${timeAndRate['seconds']} secs`;
+  //secondSpan.style.width = `34%`;
+  secondSpan.style.width = `5.5rem`;
+  secondSpan.style.textAlign = `right`;
+  const thirdSpan = document.createElement(`span`);
+  thirdSpan.textContent = `@`;
+  thirdSpan.style.width = `1.5rem`;
+  thirdSpan.style.textAlign = `center`;
+  const fourthSpan = document.createElement(`span`);
+  fourthSpan.textContent = `${timeAndRate['rate']}`
+  //fourthSpan.style.width = `47%`;
+  fourthSpan.style.width = `6.5rem`;
+  fourthSpan.style.textAlign = `right`;
+  wrapperSpan.appendChild(firstSpan);
+  wrapperSpan.appendChild(secondSpan);
+  wrapperSpan.appendChild(thirdSpan);
+  wrapperSpan.appendChild(fourthSpan);
+  return wrapperSpan;
+}
+
+const loadGoogleFont = (fontName) => {
+  // Format the name for the URL (replace spaces with plus signs)
+  const formattedName = fontName.replace(/ /g, '+');
+  const link = document.createElement('link');
+
+  link.rel = 'stylesheet';
+  link.href = `https://fonts.googleapis.com/css?family=${fontName}&display=swap`;
+
+  document.head.appendChild(link);
+}
+
 
 const changeFont = () => {
   let newFont = document.getElementById(`font`).value;
+  loadGoogleFont(newFont)
   document.getElementsByTagName(`main`)[0].style.fontFamily = newFont;
 }
 
